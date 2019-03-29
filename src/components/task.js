839e5c56bb8d8c44c3024dca9e0db656348cd2a6
mUbaidAwan <uawan2385@gmail.com>
2019-03-30 00:51:09 +0500

    heading and text field

---
+++ src/components/task.js
@@ -0,0 +1,14 @@
+import React,{Component} from "react";
+
+class Task extends Component{
+    constructor(props){
+        super(props);
+    }
+    render(){
+        return(
+            <div>Ubaid</div>
+        )
+    }
+}
+
+export default Task;
--- src/components/taskmanager.js
+++ src/components/taskmanager.js
@@ -1,8 +1,17 @@
 import React, {Component} from "react";
+import Task from "./task"
 
 class TaskManager extends Component{
+    constructor(props){
+        super(props);
+    }
     render(){
-        return <div>hello world</div>
+        return (
+        <div>
+            <h1>Task Manager</h1>
+            <input type="text"/>
+        </div>
+        )
     }
 }
 
